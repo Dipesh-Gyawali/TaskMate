b4f08559da7c211d0d5bc9c88f248245e3379555
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-03-13 17:23:50 +0545

    Delete function added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [tasks,setTasks] = useState([
+        {id:5271, name:"Record React", completed: true},
+        {id:7825, name:"Edit React", completed: false},
+        {id:8391, name:"Watch Lectures", completed: false},
+
+    ]);
+
+    function handleDelete(id){
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Man
-        </a>
-      </header>
+        <h1>Task List</h1>
+        <ul>
+            {tasks.map((task)=>(
+                <li key={task.id}>
+                    <span>{task.id}-{task.name}</span>
+                    <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+                </li>
+            ))}
+        </ul>
     </div>
   );
 }
